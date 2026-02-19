7d7fbc9499ac466a3ccd183b71d24321af185ab3
Valeriano Chete <vjchete@gmail.com>
2026-02-19 09:49:47 -0600

    fix send receipt

--- src/Jobs/SendReceipt.php
+++ src/Jobs/SendReceipt.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use SoftlogicGT\LaravelEpayServer\Mail\CCReceipt;
+use SoftlogicGT\NeoPayLaravel\Mail\CCReceipt;
 
 class SendReceipt implements ShouldQueue
 {
