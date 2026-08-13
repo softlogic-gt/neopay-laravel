b115285a008eb5396f5424ba33dbd71a9932b0a7
Valeriano Chete <vjchete@gmail.com>
2026-08-13 11:05:38 -0600

    fixes config

--- src/config/neopay.php
+++ src/config/neopay.php
@@ -7,7 +7,7 @@ return [
     'user' => env('NEOPAY_USER'),
     'password' => env('NEOPAY_PASSWORD'),
     'redirect' => env('NEOPAY_REDIRECT'),
-    'url'  => 'https://epayserver.neonet.com.gt/api/AuthorizationPaymentCommerce/',
+    'url'  => 'https://epayserver.neonet.com.gt/',
     'url_test'  => 'https://epaytestvisanet.com.gt:4433/V3/',
     'paymentgw_ip' => '181.114.3.133',
     'paymentgw_ip_test' => '190.111.1.198',
